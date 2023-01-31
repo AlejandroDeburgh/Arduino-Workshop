53b76a87d7ac9310686e14bc769a35b960e0de96
KM <113450327+KM-2023@users.noreply.github.com>
2023-01-31 06:50:41 +0400

    Update Traffic Lights Ex4.ino

--- Traffic Lights Ex4.ino
+++ Traffic Lights Ex4.ino
@@ -1,6 +1,48 @@
 int Red=4;
 int Yellow=5;
+int Red=4;
+int Yellow=5;
 int Green=6;//Set value 6 to a variable "Green"
+void setup() {
+  // put your setup code here, to run once:
+pinMode(Green,OUTPUT); //set pin as output
+pinMode(Yellow,OUTPUT); //set pin as output
+WRITECODEHERE//set RED pin as output
+
+digitalWrite(Green,LOW); //initial state
+WRITE CODE HERE //initial state for yellow LED
+digitalWrite(Red,LOW); //initial state
+
+WRITECODEHERE//Begin serial monitor
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//Yield
+
+digitalWrite(Green,LOW);
+digitalWrite(Yellow,HIGH);
+digitalWrite(Red,LOW);
+
+Serial.println(" Light Mode : Yield ");
+WRITECODEHERE//1 SECOND DELAY
+
+// Go
+WRITECODEHERE
+WRITECODEHERE
+WRITECODEHERE
+
+Serial.println(" Light Mode : Go ");
+delay(2000);
+
+// Stop
+digitalWrite(Green,LOW);
+digitalWrite(Yellow,LOW);
+digitalWrite(Red,HIGH);
+
+Serial.println(" Light Mode : STOP ");
+delay(2000);
+}//Set value 6 to a variable "Green"
 void setup() {
   // put your setup code here, to run once:
 pinMode(Green,OUTPUT); //set pin as output
